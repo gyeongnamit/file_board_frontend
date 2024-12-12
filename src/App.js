3e9e4f0c3f6791fdb4c07da5ed4908102388ad45
gyeongnamit <gyeongnam.it.202411@gmail.com>
2024-12-13 02:10:12 +0900

    입력 데이터 관리

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, {  useState } from "react";
 function App() {
+  
+  const [formData, setFormData] = useState({
+    title: "",
+    content: "",
+    author: "",
+    file: null,
+  });
+
   return (
     <div>
     {/* 게시물 작성 폼 */}
     <form>
     <div>
       <label>제목:</label>
-      <input name="title"  required />
+      <input name="title"  required  value={formData.title}  />
     </div>
     <div>
       <label>내용:</label>
-      <textarea name="content" required  />
+      <textarea name="content" required   value={formData.content}/>
     </div>
     <div>
       <label>작성자:</label>
-      <input name="author" required  />
+      <input name="author" required  value={formData.author} />
     </div>
     <div>
       <label>첨부 파일:</label>
       <input type="file" />
     </div>
-    <button type="submit">게시물 작성</button>
+    <button>게시물 작성</button>
   </form>
   </div>
   );
