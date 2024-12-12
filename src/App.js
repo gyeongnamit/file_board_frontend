4a19d19269c3895402802cf6aa5b33e71eefda95
gyeongnamit <gyeongnam.it.202411@gmail.com>
2024-12-13 01:39:02 +0900

    게시물 HTML 추가

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {/* 게시물 작성 폼 */}
+    <form>
+    <div>
+      <label>제목:</label>
+      <input name="title"  required />
     </div>
+    <div>
+      <label>내용:</label>
+      <textarea name="content" required  />
+    </div>
+    <div>
+      <label>작성자:</label>
+      <input name="author" required  />
+    </div>
+    <div>
+      <label>첨부 파일:</label>
+      <input type="file" />
+    </div>
+    <button type="submit">게시물 작성</button>
+  </form>
+  </div>
   );
 }
 
